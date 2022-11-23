16f148b94fce723ee422a7f7ebfd28c2f752d4c4
Tom <tomfyfe85@gmail.com>
2022-11-23 14:43:44 +0000

    phase2_9

--- phase2/example-3/example-3/messageView.js
+++ phase2/example-3/example-3/messageView.js
@@ -2,9 +2,11 @@ class MessageView {
   constructor() {
     this.buttonEl = document.querySelector('#show-message-button');
     this.hideButtonEl = document.querySelector('#hide-message-button');
-    
+   
+
     this.buttonEl.addEventListener('click', () => {
        this.displayMessage();
+
     });
     
     this.hideButtonEl.addEventListener('click', () => {
@@ -14,10 +16,17 @@ class MessageView {
 
   displayMessage() {
     const newDiv = document.createElement('div');
+    const newInput = document.querySelector('#message-input').value;
+    console.log(newInput);
+
     newDiv.id = 'message'
-    newDiv.innerText = 'This message bahhhh'
+    newDiv.innerText = newInput;
+
+   document.querySelector('#message-input').value = '';
+    
     const mainContainerEl = document.querySelector('#main-container')
     mainContainerEl.append(newDiv)
+    
     console.log('Thanks for clicking the add button!');
   }
 
--- phase2/example-3/example-3/messageView.test.js
+++ phase2/example-3/example-3/messageView.test.js
@@ -8,10 +8,17 @@ const MessageView = require('./messageView');
 describe('MessageView', () => {
   it('clicking button shows msg', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
+   
     const view = new MessageView();
     const buttonEl = document.querySelector('#show-message-button');
+    const inputEl = document.querySelector('#message-input');
+
+    inputEl.value = "Some text";
+    
     buttonEl.click();
+  
     expect(document.querySelector('#message')).not.toBeNull();
+    expect(document.querySelector('#message').innerText).toEqual('Some text');
   });
  
   it('clicking button removes msg', () => {
